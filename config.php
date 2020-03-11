400424eb417c32b7f102471eab5edc9831fa7253
Sean Connolly <seanconnolly@ufl.edu>
2020-03-11 19:43:56 -0400

    First commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', '34.68.183.29');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'admin');
+   define('DB_DATABASE', 'users1');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
